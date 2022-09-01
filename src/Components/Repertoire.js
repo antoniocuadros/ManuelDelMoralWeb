e0e65c2d438c1b2b1a0752d6008f6dc1ecd4164e
Antonio Cuadros Lapresta <antculap@gmail.com>
2022-09-01 12:55:04 +0200

    Arreglo margen repertorio

--- src/Components/Repertoire.js
+++ src/Components/Repertoire.js
@@ -2,45 +2,47 @@ import './Repertoire.css';
 
 function Repertoire() {
   return (
-    <div className="RepertoireContainer" id='repertoire'>
-      <p className="RepertoireTitle">Repertoire</p>
+    <div className="RepertoireContainerMargin" id='repertoire'>
+      <div className='RepertoireContainer'>
+        <p className="RepertoireTitle">Repertoire</p>
 
-      <div className='RepertoireList'>
-        <div className='RepertoireItem'>
-          <div className='RepertoireItemAuthor'>
-            <p >Paganini</p>
+        <div className='RepertoireList'>
+          <div className='RepertoireItem'>
+            <div className='RepertoireItemAuthor'>
+              <p >Paganini</p>
+            </div>
+            <div className='RepertoireItemTitle'>
+              <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
+            </div>
           </div>
-          <div className='RepertoireItemTitle'>
-            <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
-          </div>
-        </div>
 
-        <div className='RepertoireItem'>
-          <div className='RepertoireItemAuthor'>
-            <p >Paganini</p>
+          <div className='RepertoireItem'>
+            <div className='RepertoireItemAuthor'>
+              <p >Paganini</p>
+            </div>
+            <div className='RepertoireItemTitle'>
+              <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
+            </div>
           </div>
-          <div className='RepertoireItemTitle'>
-            <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
-          </div>
-        </div>
 
-        <div className='RepertoireItem'>
-          <div className='RepertoireItemAuthor'>
-            <p >Paganini</p>
+          <div className='RepertoireItem'>
+            <div className='RepertoireItemAuthor'>
+              <p >Paganini</p>
+            </div>
+            <div className='RepertoireItemTitle'>
+              <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
+            </div>
           </div>
-          <div className='RepertoireItemTitle'>
-            <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
-          </div>
-        </div>
 
-        <div className='RepertoireItem'>
-          <div className='RepertoireItemAuthor'>
-            <p >Paganini</p>
+          <div className='RepertoireItem'>
+            <div className='RepertoireItemAuthor'>
+              <p >Paganini</p>
+            </div>
+            <div className='RepertoireItemTitle'>
+              <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
+            </div>
           </div>
-          <div className='RepertoireItemTitle'>
-            <p >Rachmaninoff Rhapsody on a Theme Op. 43</p>
-          </div>
-        </div>
+        </div>        
       </div>
     </div>
   );
