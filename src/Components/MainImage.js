dcdb0ae3f2c4557ecf3536335dffd0a0dd8754fd
Antonio <antculap@gmail.com>
2023-07-11 19:34:55 +0200

    adjust image

--- src/Components/MainImage.js
+++ src/Components/MainImage.js
@@ -3,7 +3,7 @@ import image from '../images/image1.jpg';
 
 function MainImage() {
   return (
-    <img src={image} alt="Manuel Del Moral with piano"/>
+    <img className="image" src={image} alt="Manuel Del Moral with piano"/>
   );
 }
 
