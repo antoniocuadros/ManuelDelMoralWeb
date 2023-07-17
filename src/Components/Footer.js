1d2d9bdb08cb440c67bbeb690c6903bec96ab965
Antonio Cuadros Lapresta <antculap@gmail.com>
2023-07-17 16:47:04 +0200

    Adds animations to about me

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,10 @@
-//import logo from './logo.svg';
 import './Footer.css';
 
 function Footer() {
   return (
     <div className="FooterMainContainer">
       <p>Manuel Del Moral Lapresta: Solo Pianist</p>
-      <p>examplemail@gmail.com</p>
+      <p className='Mail'>examplemail@gmail.com</p>
     </div>
   );
 }
