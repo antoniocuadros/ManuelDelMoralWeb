c8999fbf3d8e2a1cfb9e91551eb2e051cf4129c3
Antonio Cuadros Lapresta <antculap@gmail.com>
2023-07-17 17:03:25 +0200

    Adds animations to about me

---
+++ src/Components/Media.js
@@ -0,0 +1,21 @@
+import './Media.css';
+
+function Media() {
+  return (
+    <div className='back2'>
+      <div className="AboutMeContainer">
+        <div className='AboutMeHeader'>
+              <p data-aos="zoom-in" data-aos-duration="1000" className="AboutMeTitle"id="aboutme">Media</p>
+        </div>
+        <div className="AboutMeTextContainer">
+          <div className='AboutMeTextSecondColumn'>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default Media;
